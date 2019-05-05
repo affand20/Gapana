465bb06bffef3c67e4c9d4e50752d72608f4c458
Aldino Haryo Pandu <aldino.haryop@gmail.com>
2019-05-05 21:06:08 +0700

    Intent Pengaturan Done. Tentang Done. Tambah 2 Class dan XML untuk ExpendableListView.

--- app/src/main/java/id/trydev/gapana/Pengaturan/PanduanActivity.java
+++ app/src/main/java/id/trydev/gapana/Pengaturan/PanduanActivity.java
@@ -2,10 +2,20 @@ package id.trydev.gapana.Pengaturan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import id.trydev.gapana.R;
 
 public class PanduanActivity extends AppCompatActivity {
+
+    ExpandableListView expandableListView;
+     HashMap<String, List<String>> listChild;
+     List<String> listHeader;
+     CustomAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/id/trydev/gapana/Pengaturan/PengaturanActivity.java
+++ app/src/main/java/id/trydev/gapana/Pengaturan/PengaturanActivity.java
@@ -1,12 +1,17 @@
 package id.trydev.gapana.Pengaturan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import id.trydev.gapana.R;
 
 public class PengaturanActivity extends AppCompatActivity {
+
+//    TextView faqBtn, tentangBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,24 @@ public class PengaturanActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setTitle("Pengaturan");
+
+        TextView faqBtn = (TextView) findViewById(R.id.faqBtn);
+        faqBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(getBaseContext(), PanduanActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView tentangBtn = (TextView) findViewById(R.id.tentangBtn);
+        tentangBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(getBaseContext(), TentangActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
